2eb394b1569ed1c6ff04a4ece5e257be15fcefdd
Jason James <jasongjames2012@hotmail.com>
2020-09-20 14:09:46 +0100

    Fix login card centre alignment

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "antd/es/layout";
-import { Card } from "antd";
+import { Card , Row} from "antd";
 import metamaskIcon from "../images/metamask.svg";
 import KeepHelperLogoText from "../images/keephelper_text_2.svg";
 const { Content } = Layout;
@@ -27,26 +27,30 @@ export function Connect(props) {
           style={{ padding: 24, minHeight: 360, textAlign: "center" }}
         >
           <div className="sidebar-logo"  ><img src={KeepHelperLogoText} height={180} style={{marginTop: '5rem'}}></img></div>
-          <Card
-            onClick={connect}
-            className={'green-shadow'}
-            hoverable
-            style={{ width: 450, margin: "5% 38% 0 38%", borderRadius: '0px' }}
-          >
-            <img
-              alt="metmask icon"
-              src={metamaskIcon}
-              style={{ width: 80, paddingBottom: "2rem" }}
-            />
-            <Meta
-              title={
-                window.web3
-                  ? "Connect With MetaMask"
-                  : "Please Install MetaMask"
-              }
-              description="MetaMask is crypto wallet & gateway to blockchain apps"
-            />
-          </Card>
+          <Row type="flex" justify="center" align="middle">
+            <Card
+                onClick={connect}
+                className={'green-shadow'}
+                hoverable
+                style={{ width: 450, margin: "5% 38% 0 38%", borderRadius: '0px' }}
+            >
+              <img
+                  alt="metmask icon"
+                  src={metamaskIcon}
+                  style={{ width: 80, paddingBottom: "2rem" }}
+              />
+              <Meta
+                  title={
+                    window.web3
+                        ? "Connect With MetaMask"
+                        : "Please Install MetaMask"
+                  }
+                  description="MetaMask is crypto wallet & gateway to blockchain apps"
+              />
+            </Card>
+          </Row>
+
+
         </div>
       </Content>
     </Layout>
